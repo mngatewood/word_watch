908aa18888447762f713f34b76d5705354706589
Michael Gatewood <michael@mngatewood.com>
2019-02-05 10:28:39 -0700

    Add post words * add click event to break down button * add post string functions * style components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   getTopWord();
+  $("#break-down").on("click", () => {submitString()});
 })
 
 const getTopWord = () => {
@@ -13,10 +14,36 @@ const getTopWord = () => {
     .catch(error => console.error({ error }))
 }
 
+const postString = (string) => {
+  let url = `https://wordwatch-api.herokuapp.com/api/v1/words`
+  let options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ word: { value: string } })
+  }
+
+  return fetch(url, options)
+    .then(response => response.json())
+    .then(result => { console.log(result) })
+    .catch(error => console.error({ error }))
+}
+
 const renderTopWord = (apiResponse) => {
   let word = Object.keys(apiResponse.word)[0];
   let count = apiResponse.word[word];
 
   $("#top-word-value").text(word)
   $("#top-word-count").text(count)
+}
+
+const submitString = () => {
+  let wordArray = parseString($("#word-text-area").val())
+  wordArray.forEach(word => {
+    postString(word);
+  })
+}
+
+const parseString = (string) => {
+  let words = string.replace(/[^\w\s]/gi, '');
+  return words.trim().split(' ');
 }
